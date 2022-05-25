9f24ee339f31f689145a3c211916d88c2f966953
namhyung123 <namhyung123@pusan.ac.kr>
2022-05-25 10:06:54 +0900

    Add Append and Append_Left function
    Append is insert node at last position which is _tail.
    Append_Left is insert node at first position which is next to _headNode.

--- libs/linkedlist.c
+++ libs/linkedlist.c
@@ -17,22 +17,58 @@ void print() {
 }
 void print_file(FILE* stream);
 void clear();
-Node* append_left(size_t n, char new_data[n]);
-Node* insert_after(Node* cur_node, Node* new_node);
-Node* append(size_t n, char new_data[n]) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = (char*)malloc(sizeof(char)*n);
-    strcpy(newNode->data, new_data);
-    if (_cur_node == NULL)  // first add
-        _head = _tail = newNode; // head, tail modified
-    else {
-        _cur_node->next = newNode;
-        newNode->prev = _cur_node;
+
+
+
+Node* append_left(size_t n, char new_data[n]){
+	//재생 목록의 첫 번째 위치에 노래를 추가함
+    Node* newNode;
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = new_data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    //printf("%s\n", newNode->data);
+    if(empty()){
+        _head = newNode;
         _tail = newNode;
     }
-    _cur_node = newNode;
+    else {
+        newNode->next = _head;
+        _head->prev = newNode;
+        _head = newNode;
+    }
+    //printf("%s\n", _head->data);
+    return newNode;	
+};
+
+
+
+Node* insert_after(Node* cur_node, Node* new_node);
+
+
+
+Node* append(size_t n, char new_data[n]) {
+        //연결 리스트 마지막에 노드추가
+    Node* newNode;
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = new_data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    if(empty()){
+        _head = newNode;
+        _tail = newNode;
+    }
+    else{
+        _tail->next = newNode;
+        newNode->prev = _tail;
+        _tail = newNode;
+    }
     return newNode;
 }
+
+
+
 Node* delete_node(Node* cur_node);
 Node* delete(char* data);
 Node* get_node(size_t index);
