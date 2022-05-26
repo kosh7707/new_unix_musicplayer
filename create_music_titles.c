7e97381065442640b6275aaf0aa1d163e3e75b79
chagieun <chagieun@gmail.com>
2022-05-26 15:55:15 +0900

    create_music_titles

---
+++ create_music_titles.c
@@ -0,0 +1,15 @@
+#include "textfilewriter.h"
+
+void create_music_titles(FILE* stream){
+	stream = fopen("title.txt", "w+");
+
+        int num;
+        scanf("%d", &num);
+        for(int i=0; i<=num; i++){
+                write_file("title.txt");
+        }
+	stream = fopen("title.txt", "a+");
+	read_file("title.txt");
+        fclose(stream);
+
+}
